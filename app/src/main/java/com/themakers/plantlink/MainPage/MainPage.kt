f9f7f5d5cf69d25ad6cdd9c2d819fd4bbcda87fa
CameronRotering <CARotering@student.fullsail.edu>
2024-10-09 16:19:52 -0400

    Updated dependencies: activityCompose, compiler, lifecycleRuntimeCompose, material, material3, navigationCompose, uiTestJunit4. Adaptive size of grid increased by 25 dp for slightly bigger phone displays.

--- app/src/main/java/com/themakers/plantlink/MainPage/MainPage.kt
+++ app/src/main/java/com/themakers/plantlink/MainPage/MainPage.kt
@@ -7,18 +7,14 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.outlined.Settings
@@ -34,7 +30,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -214,7 +209,7 @@ fun MainPage(
             horizontalArrangement = Arrangement.Start,
             contentPadding = padding,
             state = lazyGridState,
-            columns = GridCells.Adaptive(150.dp)
+            columns = GridCells.Adaptive(175.dp)
         ) {
             items(plantDeviceList.size) { deviceIndex ->
                 DeviceCard(
