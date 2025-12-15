b3761f8143849d0fcaf8ca905e9e5f3a639e5411
Cameron Rotering <crotering@fullsail.com>
2025-12-15 15:22:19 -0500

    FAB button for development to add a new devicecard.

--- app/src/main/java/com/themakers/plantlink/MainPage/MainPage.kt
+++ app/src/main/java/com/themakers/plantlink/MainPage/MainPage.kt
@@ -2,6 +2,7 @@ package com.themakers.plantlink.MainPage
 
 import android.Manifest
 import android.content.Context
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -21,6 +22,7 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +39,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -66,6 +70,8 @@ fun MainPage(
     permissionLauncher: ActivityResultLauncher<Array<String>>,
     hasBTPermission: Boolean
 ) {
+    val density = LocalDensity.current
+
     val lazyGridState = rememberLazyGridState()
     var health by remember { mutableStateOf(0) }
 
@@ -125,6 +131,16 @@ fun MainPage(
             )
         },
         bottomBar = { BottomToolBar(navController = navController) },
+        floatingActionButton = {
+            FloatingActionButton(
+                { plantDeviceList += PlantDevice("00:00:00:00:00:00", "Galaxy Petunia", "1", "10") }
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.outline_add_2_24),
+                    contentDescription = "Add Plant (DEVELOPMENT)"
+                )
+            }
+        },
         contentWindowInsets = WindowInsets.safeContent // Safe content so no content is hidden under system things like camera AND is interactive
     ) { padding ->
         Column(
@@ -185,7 +201,13 @@ fun MainPage(
                 items(plantDeviceList.size) { deviceIndex ->
                     DeviceCard(
                         modifier = Modifier
-                            .padding(deviceBoxPadding),
+                            .onGloballyPositioned { coordinates ->
+                                val widthInDp = with(density) { coordinates.size.width.toDp() }
+                                val heightInDp = with(density) { coordinates.size.height.toDp() }
+
+                                Log.w("CARDSIZE", "${widthInDp} x ${heightInDp}")
+                            },
+                            //.padding(deviceBoxPadding),
                         context = context,
                         navController = navController,
                         plantViewModel = plantViewModel,
